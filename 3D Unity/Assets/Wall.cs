a6c9ab8519443e8baabd48df0477fbbf0009967c
LEE <fta350@gmail.com>
2019-02-12 14:33:28 +0900

    unity 예제 minigame (벽넘기?) 제작

---
+++ 3D Unity/Assets/Wall.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour 
+{
+    public float speed;
+    public float delete_time;
+
+	// Use this for initialization
+	void Start () 
+    {
+        Destroy(gameObject, delete_time);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        transform.Translate(speed * Time.deltaTime, 0, 0);
+	}
+}
